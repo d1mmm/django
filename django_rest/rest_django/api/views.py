95255bc0645b5403e9b2743f78136e4e7d3e956e
Dima <zoromamid1999@gmail.com>
2022-11-11 11:28:57 +0200

    First commit

---
+++ django_rest/rest_django/api/views.py
@@ -0,0 +1,51 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Item
+from rest_framework.serializers import ListSerializer
+
+from rest_framework import serializers
+from rest_framework import status
+
+# @api_view(['GET'])
+# def view_items(request):
+    
+#     # checking for the parameters from the URL
+#     if request.query_params:
+#         items = Item.objects.filter(**request.query_param.dict())
+#     else:
+#         items = Item.objects.all()
+  
+#     # if there is something in items else raise error
+#     if items:
+#         data = ItemSerializer(items)
+#         return Response(data)
+#     else:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def ApiOverview(request):
+    api_urls = {
+        'all_items': '/',
+        'Search by Category': '/?category=category_name',
+        'Search by Subcategory': '/?subcategory=category_name',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/item/pk/delete'
+    }
+  
+    return Response(api_urls)
+
+
+@api_view(['POST'])
+def add_items(request):
+    item = ListSerializer(data=request.data)
+  
+    # validating for already existing data
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+  
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
